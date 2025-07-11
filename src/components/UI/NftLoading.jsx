7943166bc8821f42487cc513258b202aa2ebab25
nanahira727 <pixelisglory@gmail.com>
2025-07-11 16:15:57 -0700

    Explore items now takes data from the API.

---
+++ src/components/UI/NftLoading.jsx
@@ -0,0 +1,14 @@
+const NftLoading = () => {
+  return (
+    <div
+      className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
+      style={{ display: "block", backgroundSize: "cover" }}
+    >
+      <div className="nft__item nft-loading">
+        <div className="nft__item_wrap"></div>
+      </div>
+    </div>
+  );
+};
+
+export default NftLoading;
